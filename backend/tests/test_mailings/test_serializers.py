03a018085cc2b322cf83458f3e25c8c9752bcb18
john <izolotavin99@gmail.com>
2022-11-17 12:43:43 +0300

    tests.test_mailings added

---
+++ backend/tests/test_mailings/test_serializers.py
@@ -0,0 +1,38 @@
+##################################
+#    TEST's ClientTagSerializer    #
+##################################
+
+class TestClientTagSerializer:
+    pass
+
+
+###############################
+#    TEST's ClientSerializer   #
+###############################
+
+class TestClientSerializer:
+    pass
+
+
+###########################################
+#    TEST's MailingStatisticSerializer    #
+###########################################
+
+class TestMailingStatisticSerializer:
+    pass
+
+
+##################################
+#    TEST's MessageSerializer    #
+##################################
+
+class TestMessageSerializer:
+    pass
+
+
+##################################
+#    TEST's MailingSerializer    #
+##################################
+
+class TestMailingSerializer:
+    pass
---
+++ backend/tests/test_mailings/test_validators.py
@@ -0,0 +1,64 @@
+
+####################################
+#    TEST's ClientTagValidator    #
+####################################
+
+class TestClientTagValidator:
+    pass
+
+
+########################################
+#    TEST's PhoneNumberValidator    #
+########################################
+class TestPhoneNumberValidator:
+    pass
+
+
+####################################
+#    TEST's RegionCodeValidator    #
+####################################
+class TestRegionCodeValidator:
+    pass
+
+
+#################################
+#    TEST's MailingValidator    #
+#################################
+class TestMailingValidator:
+    pass
+
+
+##########################################
+#    TEST's MailingStartTimeValidator    #
+##########################################
+class TestMailingStartTimeValidator:
+    pass
+
+
+########################################
+#    TEST's MailingEndTimeValidator    #
+########################################
+class TestMailingEndTimeValidator:
+    pass
+
+
+########################################
+#    TEST's MailingFiltersValidator    #
+########################################
+class TestMailingFiltersValidator:
+    pass
+
+
+#############################################
+#    TEST's MailingTimeIntervalValidator    #
+#############################################
+class TestMailingTimeIntervalValidator:
+    pass
+
+
+##############################################
+#    TEST's MailingUpdateStatusFValidator    #
+##############################################
+class TestMailingUpdateStatusFValidator:
+    pass
+
